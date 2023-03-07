ba3f879d7c0a7c0ac77912262550ed3334c9247b
varunmsd99 <varunpelluru@gmail.com>
2023-03-07 11:38:07 +0530

    first commit

--- App.js
+++ App.js
@@ -1,1 +1,30 @@
-console.log("Hello Namaste React JS");
+import React from "react";
+import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+    "h1",
+    {
+        id:"heading1"
+    },
+    "H1 from parcel"
+);
+const heading1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading1"
+  );
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading, heading1]
+  );
+
+  // create root using createRoot
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  // passing react element inside root
+  root.render(container);
